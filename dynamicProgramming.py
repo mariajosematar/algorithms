fdfe99a9ba077ec7c6f3b49338bb0b925b1b1e26
María Jose <80407624+mariajosematar@users.noreply.github.com>
2024-04-08 21:28:50 +0200

    Update dynamicProgramming.py

--- dynamicProgramming.py
+++ dynamicProgramming.py
@@ -1,5 +1,4 @@
-# Ejercicio: Dado un conjunto de monedas con valores [1, 5, 10, 25] y una cantidad, encuentra el número mínimo de monedas necesarias para formar esa cantidad.
-
+# Exercise: Given a set of coins with values ​​[1, 5, 10, 25] and a quantity, find the minimum number of coins needed to form that quantity.
 def min_coins(coins, amount):
     dp = [float('inf')] * (amount + 1)
     dp[0] = 0
@@ -8,7 +7,7 @@ def min_coins(coins, amount):
             dp[i] = min(dp[i], dp[i - coin] + 1)
     return dp[amount]
 
-# Ejemplo
+# Example:
 coins = [1, 5, 10, 25]
 amount = 48
 
@@ -16,5 +15,5 @@ print(min_coins(coins, amount))
 
 # Output: 6 (48 = 25 + 10 + 10 + 1 + 1 + 1)
 
-# Complejidad temporal: O(n * m), donde n es la cantidad y m es el número de tipos de monedas.
-# Esto se debe a que el algoritmo llena una matriz de tamaño n * m y cada celda de la matriz se llena una vez. 
+# Time complexity: O(n * m), where 'n' is the quantity and m is the number of types of coins.
+# This is because the algorithm fills an array of size n*m and each cell of the array is filled once.
