27c794c888202cc997a88ceef2eb84d4aba930cf
María Jose <80407624+mariajosematar@users.noreply.github.com>
2024-04-08 21:25:03 +0200

    Update bruteForceSearch.py

--- bruteForceSearch.py
+++ bruteForceSearch.py
@@ -1,18 +1,17 @@
-# Ejercicio: Dado un conjunto de números, encuentra el par de números cuya suma sea igual a un objetivo dado.
-
+# Exercise: Given a set of numbers, find the pair of numbers whose sum equals a given objective.
 def two_sum(nums, target):
     for i in range(len(nums)):
         for j in range(i + 1, len(nums)):
             if nums[i] + nums[j] == target:
                 return [i, j]
 
-# Ejemplo
+# Example:
 nums = [2, 7, 11, 15]
 target = 9
 
 print(two_sum(nums, target)) 
 
-# Output: [0, 1] (índices de los números 2 y 7)
+# Output: [0, 1] (indices of numbers 2 y 7)
 
-# Complejidad temporal: O(n^2), donde n es el número de elementos en la lista.
-# Esto se debe a que el algoritmo realiza un bucle anidado para cada elemento en la lista.
+# Time complexity: O(n^2), where 'n' is the number of elements in the list.
+# When the number of elements in a list is 'n', the algorithm performs a nested loop 'n' times.
