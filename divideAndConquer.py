d71611ee8a8acb41b7d63d19ea599e1d36afd892
María Jose <80407624+mariajosematar@users.noreply.github.com>
2024-04-08 21:27:16 +0200

    Update divideAndConquer.py

--- divideAndConquer.py
+++ divideAndConquer.py
@@ -1,5 +1,4 @@
-# Ejercicio: Implementa el algoritmo de búsqueda binaria para encontrar un elemento en una lista ordenada.
-
+# Exercise: Implement the binary search algorithm to find an element in an ordered list.
 def binary_search(arr, target):
     left, right = 0, len(arr) - 1
     while left <= right:
@@ -12,13 +11,13 @@ def binary_search(arr, target):
             right = mid - 1
     return -1
 
-# Ejemplo
+# Example:
 arr = [1, 3, 5, 7, 9, 11, 13]
 target = 7
 
 print(binary_search(arr, target))  
 
-# Output: 3 (el índice del elemento 7)
+# Output: 3 (index element = 7)
 
-# Complejidad temporal: O(log n), donde n es el número de elementos en la lista.
-# Esto se debe a que el algoritmo divide repetidamente la lista a la mitad en cada paso de la búsqueda.
+# Time complexity: O(log n), where 'n' is the number of elements in the list.
+# This is because the algorithm repeatedly divides the list in half during each step of the search.
